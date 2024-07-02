266cb4eef8ff06c6a82248e831ca9e9177855675
garyxcj <shudezx@gmail.com>
2024-07-02 16:43:56 -0700

    Add scenario_list example for hallucination

---
+++ mmdt/perspectives/hallucination/scenario_list.py
@@ -0,0 +1,10 @@
+scenario_list = {
+    "text_to_image": {
+        "counterfactual": ["attribute", "count", "identification", "spatial"],
+        "distraction": [],
+    },
+    "image_to_text": {
+        "counterfactual": ["attribute", "count", "identification", "spatial"],
+        "distraction": [],
+    },
+}
