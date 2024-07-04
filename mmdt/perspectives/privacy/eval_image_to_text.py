feff8464ed3a9e14dc7bff9d0079ab71cf849585
1mocat <3210103568@zju.edu.cn>
2024-07-04 04:57:45 +0000

    privacy scenario task

--- mmdt/perspectives/privacy/eval_image_to_text.py
+++ mmdt/perspectives/privacy/eval_image_to_text.py
@@ -12,13 +12,7 @@ if __name__ == '__main__':
     parser.add_argument('--scenario', type=str, default="natural", help='Specify the scenario')
     parser.add_argument('--task', type=str, default="identification", help='Specify the task to execute')
     args = parser.parse_args()
-
+    
     if(args.scenario=="location"):
         evaluate_location(args.model_id,args.task)
     #else PII
-    # 使用相对路径时确保文件在当前工作目录中
-    csv_filename = './label_sum.csv'
-
-    # Check if the CSV file exists
-    if not os.path.exists(csv_filename):
-        raise FileNotFoundError(f"The file {csv_filename} does not exist.")
