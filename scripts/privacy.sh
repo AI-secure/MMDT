406ea1337a453b0c8126332f75b52b613c88782f
1mocat <3210103568@zju.edu.cn>
2024-07-04 04:55:44 +0000

    scripts privacy

---
+++ scripts/privacy.sh
@@ -0,0 +1,11 @@
+model_id="llava-hf/llava-v1.6-mistral-7b-hf"
+scenario="location"
+task="Pri-SV-with-text"
+
+cd mmdt/perspectives
+# python privacy/eval_image_to_text.py --model_id ${model_id} --scenario ${scenario} --task ${task}
+
+model_id="gpt-4o-2024-05-13"
+task="Pri-4Loc-SV-without-text"
+python privacy/eval_image_to_text.py --model_id ${model_id} --scenario ${scenario} --task ${task}
+
