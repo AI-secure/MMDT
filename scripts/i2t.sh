672b0c5c0309d6a6b3619210aeceed2bc3572284
Chejian Xu <shudezx@gmail.com>
2025-03-09 00:47:35 -0600

    Update README

---
+++ scripts/i2t.sh
@@ -0,0 +1,6 @@
+bash scripts/safety_i2t.sh $1
+bash scripts/hallucination_i2t.sh $1
+bash scripts/fairness_i2t.sh $1
+bash scripts/privacy_i2t.sh $1
+bash scripts/adv_i2t.sh $1
+bash scripts/ood_i2t.sh $1
